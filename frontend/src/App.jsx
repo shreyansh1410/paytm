4b78a9d597d8a5e0e20ef29c3289256979afe9c9
Shreyansh Shukla <shreyansh.14010@gmail.com>
2024-10-16 22:42:06 +0530

    installed react-router-dom and initialised routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,21 @@
+import { useNavigate, BrowserRouter, Route, Routes } from "react-router-dom";
+import Send from "./components/Send.jsx";
+import Dashboard from "./components/Dashboard.jsx";
+import Signup from "./components/Signup.jsx";
+import Signin from "./components/Signin.jsx";
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={"Hello"} />
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/signin" element={<Signin/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/send" element={<Send />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
