405aa658d0a7de4a1464a3b958d031bbce370622
Shreyansh Shukla <shreyansh.14010@gmail.com>
2024-10-19 22:55:08 +0530

    added send money component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
 import { useNavigate, BrowserRouter, Route, Routes } from "react-router-dom";
-import Send from "./pages/Send.jsx";
+import {Send} from "./pages/Send.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import Signup from "./pages/Signup.jsx";
 import Signin from "./pages/Signin.jsx";
@@ -17,7 +17,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/send" element={<Send />} />
+        <Route path="/send" element={<Send name={"Harki"}/>} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/pages/Send.jsx
+++ frontend/src/pages/Send.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { Button } from "../components/Button";
+import { Heading } from "../components/Heading";
+import { InputBox } from "../components/InputBox";
 
-const Send = () => {
-  return <div></div>;
+export const Send = (user) => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-slate-300">
+      <div className="flex flex-col mx-auto max-w-screen-md text-xl bg-slate-400 p-4 rounded-md">
+        <div className="p-2">
+          <Heading heading={"Send Money"} />
+        </div>
+        <div className="flex justify-start items-center p-2">
+          <div className="mr-2 bg-slate-500 rounded-full text-2xl w-12 h-12 items-center justify-center flex">
+            {user.name[0]}
+          </div>
+          <div className="font-medium">{user.name}</div>
+        </div>
+        <div className="p-2">
+          <InputBox label={"Amount (in ₹)"} placeholder={"Amount"} />
+        </div>
+        <div className="p-2">
+          <Button buttonText={"Send"} />
+        </div>
+      </div>
+    </div>
+  );
 };
-
-export default Send;
